01afff133695eb9186123287077bb082ca57fc05
aleja <92136921+mariather@users.noreply.github.com>
2022-10-08 21:11:01 -0700

    try to build db again

---
+++ db/query.session.sql
@@ -0,0 +1,7 @@
+USE company_db;
+INSERT INTO departments (department_name)
+VALUES ("bla");
+INSERT INTO roles (title, salary, department_id)
+VALUES ('blablabla', 1738, 5);
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ('lala', 'doremi', 12, null);
