5b1b16351df1d4a3e271a9ce3f572cf83c3b4630
aleja <92136921+mariather@users.noreply.github.com>
2022-10-08 21:51:25 -0700

    update readme

--- db/query.session.sql
+++ db/query.session.sql
@@ -2,6 +2,6 @@ USE company_db;
 INSERT INTO departments (department_name)
 VALUES ("bla");
 INSERT INTO roles (title, salary, department_id)
-VALUES ('blablabla', 1738, 5);
+VALUES ('blablabla', 1500, 5);
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ('lala', 'doremi', 12, null);
+VALUES ('lala', 'doremi', 5, null);
